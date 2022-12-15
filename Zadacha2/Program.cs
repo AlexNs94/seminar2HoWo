ed15c555c909a29229807a10d41ba456407c4dc5
alexns <alexns9419@yandex.ru>
2022-12-15 21:09:58 +0500

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+Console.WriteLine(" Введите число ");
+
+int number = int.Parse(Console.ReadLine()); 
+
+
+
+if (number < 0) 
+    
+    Console.WriteLine(" программа не работает с отрицательными числами ");
+
+else 
+{
+    if (number < 100 && number >= 0)
+     
+        Console.WriteLine($" в числе  {number} нет третей цифры ");
+      
+    else
+       {
+            while (number > 1000)
+                { 
+                    number = number / 10;
+                }
+                int ostatok = number % 10;
+        
+                Console.WriteLine($" в числе  {number} третья цифра {ostatok}");
+        };
+};
