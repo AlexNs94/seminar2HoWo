8aa7cd387b4ca51d89a71b1098e936aeaf58b412
alexns <alexns9419@yandex.ru>
2022-12-15 21:17:13 +0500

    Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели: ");
+
+int day = int.Parse(Console.ReadLine());
+ 
+if (day > 5 && day < 8)
+
+    Console.Write("выходной");
+
+else
+     if (day > 0 && day < 6)
+    
+        Console.Write("рабочий");
+
+    else
+        Console.Write("некорректное число");
