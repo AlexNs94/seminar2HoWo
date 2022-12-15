5b5c66547c419ac855dd52bdcdeb31abc162fb3c
alexns <alexns9419@yandex.ru>
2022-12-15 20:47:11 +0500

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число");
+
+int number = int.Parse(Console.ReadLine());
+
+int ostatok =(number/10)%10;
+
+if ((number > 999) || (number < 100))
+
+    Console.WriteLine("Введенное число не подходит под условие задачи");
+    
+else
+
+    Console.WriteLine($"вторая цифра от числа {number} это {ostatok}");
