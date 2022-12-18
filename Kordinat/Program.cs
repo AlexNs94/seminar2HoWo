376c13aad722545fd159e7b1e67d8ff78161ba1b
alexns <alexns9419@yandex.ru>
2022-12-18 23:14:54 +0500

    kordinat

---
+++ Kordinat/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int DataEntryXY(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FindQvandrant(int x, int y)
+{
+   
+    if (( x > 0) && (y > 0)) Console.WriteLine("Это первый квадрат");
+    if (( x < 0) && (y > 0)) Console.WriteLine("Это второй квадрат");
+    if (( x < 0) && (y < 0)) Console.WriteLine("Это третий квадрат");
+    if (( x > 0) && (y < 0)) Console.WriteLine("Это четвертый квадрат");
+    
+}
+
+
+int x = DataEntryXY("Введите кординаты x = ");
+//Console.WriteLine(x);
+int y = DataEntryXY("Введите кординаты y = ");
+//Console.WriteLine(y);
+FindQvandrant(x,y);
