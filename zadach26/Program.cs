75a0259c3cb2c8e8083f12e0b1b69db6ff44ad31
alexns <alexns9419@yandex.ru>
2022-12-23 00:20:01 +0500

    zadacha26

---
+++ zadach26/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+
+Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int QualytiNumber(int num)
+{
+    int  i ;
+    for (i =0; num >0 ; i++)
+    {
+       num = num / 10;
+    }
+    return i;
+}
+int FindUnitNumber = DataEntry("Введите чило :");
+int result = QualytiNumber(FindUnitNumber);
+Console.WriteLine(result);
