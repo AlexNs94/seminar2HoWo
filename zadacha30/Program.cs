a8c342e91e7a94bad4b619cc88e6aca9839f7316
alexns <alexns9419@yandex.ru>
2022-12-24 00:29:36 +0500

    zadacha30

---
+++ zadacha30/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+Console.Clear();
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[] FillArray(int[] array)
+{
+    int Length = array.Length;
+    int index = 0;
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(2);
+    }
+    return array;
+
+}
+void PrintArray(int[] Fill)
+{
+    int count = Fill.Length;
+    int i = 0;
+
+    for (i = 1; i < count; i++)
+    {
+        Console.Write(Fill[i] + " ");
+        //position++;
+    }
+}
+int number = DataEntry("Введите размер :");
+int[] array = new int[number];
+array = FillArray(array);
+PrintArray(array);
