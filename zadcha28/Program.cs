8ea79c4eb8f8c99deac2bb14ac4a55028e73e7e0
alexns <alexns9419@yandex.ru>
2022-12-23 23:16:29 +0500

    zadacha28

---
+++ zadcha28/Program.cs
@@ -0,0 +1,27 @@
+﻿//Решение в группах задач:
+//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+Console.Clear();
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int FindSum(int num)
+{
+    
+    int sum = 1;
+    for ( int i = 1 ; i <= num ; i++ )
+    {
+       sum = sum * i;
+    }
+    return sum;
+}
+//Получили число
+int number = DataEntry("Введите число A ");
+int result = FindSum(number);
+Console.WriteLine("Сумма всех чисел до А = " +result);
